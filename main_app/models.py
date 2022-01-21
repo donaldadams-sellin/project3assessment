d2fd1313298f1f97bccdf73c6f3114848d67302e
Donald Adams-Sellin <snave1993@hotmail.com>
2022-01-21 14:33:50 -0500

    complete step 3.2

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.forms import IntegerField
 
 # Create your models here.
+class Widget(models.Model):
+    description = models.CharField(max_length=50)
+    quantity = IntegerField()
