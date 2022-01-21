d84397f611895f24bc2177cee567996e3a2ed75d
Donald Adams-Sellin <snave1993@hotmail.com>
2022-01-21 15:09:50 -0500

    complete bonus

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import redirect, render
+from django.db.models import Count
 from .models import Widget
 from .forms import WidgetForm
 # Create your views here.
 def index(request):
     widgets = Widget.objects.all()
     form = WidgetForm()
-    return render(request, 'index.html', {'widgets':widgets, 'form':form})
+    total_quant = 0
+    for widget in widgets:
+        total_quant += widget.quantity
+    return render(request, 'index.html', {'widgets':widgets, 'form':form, 'total_quant': total_quant})
 
 def add_widget(request):
     widget = WidgetForm(request.POST)
